4b2f7eb477368181efa04ba03a12d2f5e5016815
hyengchan <hyengchan@gmail.com>
2021-01-15 16:34:27 +0900

    extension 을 사용하여 깔끔한 코드 작성

--- ch7_델리게이트 패턴/Delegate-ImagePicker/Delegate-ImagePicker/ViewController.swift
+++ ch7_델리게이트 패턴/Delegate-ImagePicker/Delegate-ImagePicker/ViewController.swift
@@ -7,8 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+class ViewController: UIViewController {
     @IBOutlet var imgView: UIImageView!
     
     @IBAction func pick(_ sender: Any) {
@@ -23,7 +22,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         // 이미지 피커 컨트롤러 실행
         self.present(picker, animated: false)
     }
-    
+}
+
+// MARK:- 이미지 피커 컨트롤러 델리게이트 메소드
+extension ViewController: UIImagePickerControllerDelegate {
     // 이미지 피커에서 이미지를 선택하지 않고 취소했을 때 호출되는 메소드
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         // 이미지 피커 컨트롤러 창 닫기
@@ -47,6 +49,63 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             self.imgView.image = img
         }
     }
+}
 
+// MARK:- 네비게이션 컨트롤러 델리게이트 메소드
+extension ViewController: UINavigationControllerDelegate {
+}
+
+// MARK:- 텍스트필드 델리게이트 메소드
+// 앞선 실습에서 가져온 델리게이트 메소드 구현
+extension ViewController: UITextFieldDelegate {
+    // 텍스트필드의 편집을 시작할 때 호출
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        print("텍스트 필드의 편집이 시작됩니다.")
+        return true // false를 리턴하면 편집되지 않는다.
+    }
+    
+    // 텍스트 필드의 편집이 시작된 후 호출
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("텍스트 필드의 편집이 시작되었습니다.")
+    }
+    
+    // 텍스트필드의 내용이 삭제될 때 호출
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        print("텍스트 필드의 내용이 삭제됩니다.")
+        return true // false 를 리턴하면 삭제되지 않는다.
+    }
+    
+    // 텍스트 필드의 내용이 변경될 때 호출
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        print("텍스트 필드의 내용이 \(string)으로 변경됩니다")
+        if Int(string) == nil { // 입력된 값이 숫자가 아니라면 true를 리턴
+            if (textField.text?.count)! + string.count > 10 { // 현재 텍스트 필드에 입력된 길이와 더해질 문자열 길이의 합이 10을 넘는다면 반영하지 않음
+                
+                return false
+            } else {
+                return true
+            }
+        } else { // 입력된 값이 숫자라면 false를 리턴
+            return false
+        }
+    }
+    
+    // 텍스트 필드의 리턴키가 눌러졌을 때 호출
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        print("텍스트 필드의 리턴키가 눌러졌습니다.")
+        return true
+    }
+    
+    // 텍스트 필드 편집이 종료될 때 호출
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        print("텍스트 필드의 편집이 종료됩니다.")
+        return true // false를 리턴하면 내용이 변경되지 않는다.
+    }
+    
+    // 텍스트 필드의 편집이 종료되었을 때 호출
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("텍스트 필드의 편집이 종료되었습니다.")
+    }
 }
 
