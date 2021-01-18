257f29391643ca4f29b85f68c36d9f994a9e2099
hyengchan <hyengchan@gmail.com>
2021-01-18 15:36:56 +0900

    프로토타입 셀을 이용한 테이블 뷰 실습

---
+++ ch8_테이블 뷰를 이용한 데이터 목록 구현/MyMovieChart/MyMovieChart/ListViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ListViewController.swift
+//  MyMovieChart
+//
+//  Created by 윤형찬 on 2021/01/18.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+    // 테이블 뷰를 구성할 리스트 데이터
+    var dataset = [
+        ("다크나이트", "영웅물에 철학에 음학까지 더해져 예술이 되다.", "2008-09-04", 8.95),
+        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31),
+        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19)
+    ]
+    
+    // 테이블 뷰를 구성할 리스트 데이터
+    lazy var list: [MovieVO] = {
+        var datalist = [MovieVO]()
+        for (title, desc, opendate, rating) in self.dataset {
+            let mvo = MovieVO()
+            mvo.title = title
+            mvo.description = desc
+            mvo.opendate = opendate
+            mvo.rating = rating
+            
+            datalist.append(mvo)
+        }
+        return datalist
+    }()
+    
+    override func viewDidLoad() {
+    }
+    
+    // 테이블 뷰 행의 개수를 반환하는 메소드
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.list.count
+    }
+    
+    // 테이블 뷰 행을 구성하는 메소드
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 주어진 행에 맞는 데이터 소스를 읽어온다.
+        let row = self.list[indexPath.row]
+        // 테이블 셀 객체를 직접 생성하는 대신 큐로부터 가져옴
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
+        cell.textLabel?.text = row.title
+        
+        // 서브타이틀에 데이터 연결
+        cell.detailTextLabel?.text = row.description
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        NSLog("선택된 행은 \(indexPath.row) 번째 행입니다.")
+    }
+}
---
+++ ch8_테이블 뷰를 이용한 데이터 목록 구현/MyMovieChart/MyMovieChart/MovieVO.swift
@@ -0,0 +1,17 @@
+//
+//  MovieVO.swift
+//  MyMovieChart
+//
+//  Created by 윤형찬 on 2021/01/18.
+//
+
+import Foundation
+
+class MovieVO {
+    var thumbnail: String? // 영화 섬네일 이미지 주소
+    var title: String? // 영화 제목
+    var description: String? // 영화 설명
+    var detail: String? // 상세정보
+    var opendate: String? // 개봉일
+    var rating: Double? // 평점
+}
