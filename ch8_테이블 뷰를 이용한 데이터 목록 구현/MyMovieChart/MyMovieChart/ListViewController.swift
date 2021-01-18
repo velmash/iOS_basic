823d64f9ab397aa2ec39bddb49002bce00237721
hyengchan <hyengchan@gmail.com>
2021-01-18 16:39:15 +0900

    커스텀 클래스로 프로토타입 셀의 객체 제어하기

--- ch8_테이블 뷰를 이용한 데이터 목록 구현/MyMovieChart/MyMovieChart/ListViewController.swift
+++ ch8_테이블 뷰를 이용한 데이터 목록 구현/MyMovieChart/MyMovieChart/ListViewController.swift
@@ -10,20 +10,21 @@ import UIKit
 class ListViewController: UITableViewController {
     // 테이블 뷰를 구성할 리스트 데이터
     var dataset = [
-        ("다크나이트", "영웅물에 철학에 음학까지 더해져 예술이 되다.", "2008-09-04", 8.95),
-        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31),
-        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19)
+        ("다크나이트", "영웅물에 철학에 음학까지 더해져 예술이 되다.", "2008-09-04", 8.95, "darknight.jpg"),
+        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31, "rain.jpg"),
+        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19, "secret.jpg")
     ]
     
     // 테이블 뷰를 구성할 리스트 데이터
     lazy var list: [MovieVO] = {
         var datalist = [MovieVO]()
-        for (title, desc, opendate, rating) in self.dataset {
+        for (title, desc, opendate, rating, thumbnail) in self.dataset {
             let mvo = MovieVO()
             mvo.title = title
             mvo.description = desc
             mvo.opendate = opendate
             mvo.rating = rating
+            mvo.thumbnail = thumbnail
             
             datalist.append(mvo)
         }
@@ -43,11 +44,41 @@ class ListViewController: UITableViewController {
         // 주어진 행에 맞는 데이터 소스를 읽어온다.
         let row = self.list[indexPath.row]
         // 테이블 셀 객체를 직접 생성하는 대신 큐로부터 가져옴
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
-        cell.textLabel?.text = row.title
+        //let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
         
+        /* style - basic
         // 서브타이틀에 데이터 연결
+        cell.textLabel?.text = row.title
         cell.detailTextLabel?.text = row.description
+        */
+        
+        /* style - custom
+        // 영화제목이 표시될 레이블을 title 변수로 받음
+        let title = cell.viewWithTag(101) as? UILabel
+        // 영화요약desc이 표시될 레이블을 desc 변수로 받음
+        let desc = cell.viewWithTag(102) as? UILabel
+        // 개봉일이 표시될 레이블을 opendate 변수로 받음
+        let opendate = cell.viewWithTag(103) as? UILabel
+        // 평점이 표시될 레이블을 rating 변수로 받음
+        let rating = cell.viewWithTag(104) as? UILabel
+        
+        // 데이터 소스에 저장된 값을 각 레이블 변수에 할당
+        title?.text = row.title
+        desc?.text = row.description
+        opendate?.text = row.opendate
+        rating?.text = "\(row.rating!)"
+        */
+        
+        // 커스텀 클래스로 프로토타입 셀 제어
+        // 프로토타입 셀의 클래스를 MovieCell로 연결해주었다. 이 클래스에 정의된 속성을 사용하려면 MovieCell 타입으로 캐스팅해 주어야 한다.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell") as! MovieCell
+        
+        // 데이터 소스에 저장된 값을 각 아울렛 변수에 전달
+        cell.title?.text = row.title
+        cell.desc?.text = row.description
+        cell.opendate?.text = row.opendate
+        cell.rating?.text = "\(row.rating!)"
+        cell.thumbnail.image = UIImage(named: row.thumbnail!)
         
         return cell
     }
