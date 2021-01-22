6005edd1574e5cfb689a60cde33314ddc811fe52
hyengchan <hyengchan@gmail.com>
2021-01-22 14:11:21 +0900

    오토 리사이징, 오토 레이아웃 실습

---
+++ ch14_오토 레이아웃/AutoResizing-Practice/AutoResizing-Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoResizing-Practice
+//
+//  Created by 윤형찬 on 2021/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
