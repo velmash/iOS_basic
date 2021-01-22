43bddc06c68eb5b921f9634def7e30bd4eafc283
hyengchan <hyengchan@gmail.com>
2021-01-22 11:18:34 +0900

    맵 킷 프레임워크 실습

---
+++ ch13_맵킷 프레임워크 사용하기/MyMovieChart/MyMovieChart/TheaterCell.swift
@@ -0,0 +1,17 @@
+//
+//  TheaterCell.swift
+//  MyMovieChart
+//
+//  Created by 윤형찬 on 2021/01/22.
+//
+
+import UIKit
+
+class TheaterCell: UITableViewCell {
+    // 상영관명
+    @IBOutlet var name: UILabel!
+    // 연락처
+    @IBOutlet var tel: UILabel!
+    // 주소
+    @IBOutlet var addr: UILabel!
+}
